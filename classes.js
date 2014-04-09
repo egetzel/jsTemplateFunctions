805378d34b88adb0c6928c72455e7fe887c35f04
Edaan <Edaan.Getzel@gmail.com>
2014-04-09 13:17:39 -0700

    add classes reminder

---
+++ classes.js
@@ -0,0 +1,25 @@
+Weapon = Class.extend({
+    init: function() { 
+        this._super();     
+    }
+});
+
+MachineGun = Weapon.extend({
+    init: function() {       
+        this._super();
+    }
+});
+
+
+ChainGun = Weapon.extend({});
+
+Entity = Class.extend({});
+
+Teleporter = Entity.extend({});
+
+EnergyCanister = Entity.extend({});
+
+
+
+
+
