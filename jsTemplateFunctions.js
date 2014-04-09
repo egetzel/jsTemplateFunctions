a4bc42d122e3704a3aa55581d10a5cf9a90c0252
Edaan <Edaan.Getzel@gmail.com>
2014-04-09 10:44:21 -0700

    add callback function

--- jsTemplateFunctions.js
+++ jsTemplateFunctions.js
@@ -9,3 +9,23 @@ xhr.onload= function(){
 };
 xhr.send()
 	
+// Async template function with callback functionality built in
+
+function xhrGet(reqUri, callback, type){
+	var caller = xhrGet.caller;
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", reqUri, true);
+
+	if (type){
+		xhr.responseType = type;
+	}
+	xhr.onload = function() {
+		if(callback){
+			try {
+				callback(xhr);
+			} catch(e) {
+				throw 'xhrGet failed:\n' + reqUri+ '\nException:'+e+'\nresponseText:'+xhr.responseText+'\ncaller:'+caller;
+			}
+		}
+	}
+}
