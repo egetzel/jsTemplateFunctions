ef6e1f59e7ebc7669c5ff61d3cdf30f86a41074e
Edaan <Edaan.Getzel@gmail.com>
2014-04-09 11:44:15 -0700

    debug JS

--- jsTemplateFunctions.js
+++ jsTemplateFunctions.js
@@ -7,7 +7,7 @@ xhr.open('GET','URL',true);
 xhr.onload= function(){
 	parsedJSON = JSON.parse(this.responseText);
 };
-xhr.send()
+xhr.send();
 	
 // Async template function with callback functionality built in
 
@@ -28,4 +28,22 @@ function xhrGet(reqUri, callback, type){
 			}
 		}
 	}
-}
+	xhr.send();
+};
+
+// checks password REGEX and throws an error if PW doesn't meet criteria
+document.getElementById('exampleForm').onsubmit(function(){
+	var pwREGEX = /^[A-Za-z\d]{6,8}$/
+	if(!passwordRegex.test(document.getElementById('examplePass').val)){
+		console.log('Regext did not match')
+		var notify = document.getElementById('notify');
+		if (notify === null) {
+			notify = document.createElement('p');
+			notify.textContext = 'Passwords need to be 6 to 8 letters long'
+			notify.id = 'notify'
+
+			var body = document.getElementById('body');
+			body.appendChild(notify);
+		}
+	}
+});
